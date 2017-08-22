161a7ae42ccaf6994fff6f475293be12873e5a8e
madhubpaladugu <madhu.paladugu@gmail.com>
2017-08-22 11:36:28 -0700

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,21 @@
+setwd("F:/Work/Coursera/EDA/week1")
+##Memory calculation
+round(2075259*9*8/2^{20}/1024,2)
+##0.14 GB of memory required to read the raw data file into R
+data <- read.table("household_power_consumption.txt",header = T,sep=";",stringsAsFactors = FALSE)
+data$datetime <- strptime(paste(data$Date,data$Time,sep=" "),format='%d/%m/%Y %H:%M:%S')
+data$Date <- as.Date(as.character(data$Date),format='%d/%m/%Y')
+data_sub <- subset(data,Date>="2007-02-01" & Date <= "2007-02-02")
+data_sub$Global_active_power <- as.numeric(data_sub$Global_active_power)
+
+data_sub$Sub_metering_1 <- as.numeric(data_sub$Sub_metering_1)
+data_sub$Sub_metering_2 <- as.numeric(data_sub$Sub_metering_2)
+data_sub$Sub_metering_3 <- as.numeric(data_sub$Sub_metering_3)
+par(mfrow=c(1,1))
+png("plot3.png", width=480, height=480)
+with(data_sub,plot(datetime,Sub_metering_1,type='l',xlab="",ylab="Energy sub metering"))
+lines(data_sub$datetime,data_sub$Sub_metering_2,type='l',col='red')
+lines(data_sub$datetime,data_sub$Sub_metering_3,type='l',col='blue')
+##legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3",col=c("black","red","blue"),lty='',bty="o"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+dev.off()
