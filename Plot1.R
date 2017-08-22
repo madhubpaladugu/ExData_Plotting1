ede2df3c43de0d5f76135e502580fd70b2dc5dc2
madhubpaladugu <madhu.paladugu@gmail.com>
2017-08-22 11:33:18 -0700

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+setwd("F:/Work/Coursera/EDA/week1")
+##Memory calculation
+round(2075259*9*8/2^{20}/1024,2)
+##0.14 GB of memory required to read the raw data file into R
+data <- read.table("household_power_consumption.txt",header = T,sep=";",stringsAsFactors = FALSE)
+data$datetime <- strptime(paste(data$Date,data$Time,sep=" "),format='%d/%m/%Y %H:%M:%S')
+data$Date <- as.Date(as.character(data$Date),format='%d/%m/%Y')
+data_sub <- subset(data,Date>="2007-02-01" & Date <= "2007-02-02")
+data_sub$Global_active_power <- as.numeric(data_sub$Global_active_power)
+
+png("plot1.png", width=480, height=480)
+hist(data_sub$Global_active_power,col="red",main='Global Active Power',xlab="Global Active Power (kilowatts)",ylab = "Frequency")
+dev.off()
